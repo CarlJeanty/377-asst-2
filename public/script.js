5e5018d8afb8f17f69c4df9c9a918c77f677e517
Carl Jeanty <CarlJeanty@users.noreply.github.com>
2020-11-07 15:04:36 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,21 +1,27 @@
-function convertNamesToCategories(nameCatList) {
-    // process your restaurants here!
-    const nameList = [];
-    const nameObj = {};
-    var i;
-    for (let i=0; i<nameCatList.length; i++) {
-        nameList.push(nameCatList[i].category);
-      if (!nameObj[nameList[i]]) {
-        nameObj[nameList[i]] = 0;
-      }
-      nameObj[nameList[i]] += 1;
-      console.log(restaurantList[i].category);
-    }
-    //   console.log(catObj);
-    const list = Object.keys(nameObj).map((m) => ({
-      y: nameObj[m],
-      label: m
-    }));
+const file = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
-    return list;
-  } 
+const cat = [];
+
+//const prom= fetch(file);
+//console.log(prom)
+
+//fetch(file).then(Bdata =>console.log(Bdata)); raw data no info
+
+//fetch(file)
+ //.then(Bdata => Bdata.json())
+ //.then(data => console.log(data))
+
+ fetch(file)
+ .then(Bdata => Bdata.json())
+ .then(data => cat.push(...data))
+
+ function findmatch(searchmatch,cat){
+  return cat.filter(x => {
+
+    const regex = new RegExp(searchmatch,'gi'); 
+    return x.city.match(regex)
+  } );
+  
+   
+ }
+ findmatch("col",cat)
