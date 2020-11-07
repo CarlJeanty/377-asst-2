d6318ee42ee1cb12df91e2d745f659d1f6d74db7
Carl Jeanty <CarlJeanty@users.noreply.github.com>
2020-11-07 15:53:59 -0500

    attempt 3 at js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+
+
 const file = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
 const cat = [];
@@ -11,17 +13,37 @@ const cat = [];
  //.then(Bdata => Bdata.json())
  //.then(data => console.log(data))
 
- fetch(file)
+fetch(file)
  .then(Bdata => Bdata.json())
  .then(data => cat.push(...data))
 
- function findmatch(searchmatch,cat){
+ function findMatches(searchmatch,cat){
   return cat.filter(x => {
-
-    const regex = new RegExp(searchmatch,'gi'); 
-    return x.city.match(regex)
+    
+    const regex = new RegExp(searchmatch,'gi');
+    return x.city.name(regex) 
   } );
   
    
  }
- findmatch("col",cat)
+
+
+/*
+ function displaymatches(){
+   const matcharr = findmatch(this.value, cities);
+   const html = matcharr.map(x=>{
+    return `
+    <li>
+       <span class="rest">${category.city},${category.state},${restau.name}</span>
+       <span class="loc">${$category.address_line_1}</span>
+    </li>
+    `;
+   })
+ }
+
+ const searchInp = document.querySelector('.Userinput');
+
+ 
+ searchInp.addEventListener('change',displaymatches);
+ searchInp.addEventListener('keyup',displaymatches)
+ */
