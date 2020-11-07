cec19ae4dc705deb301d8abb404ff8eaea51da03
aarthiganesh55 <61809010+aarthiganesh55@users.noreply.github.com>
2020-11-07 15:58:35 -0500

    updates

--- public/script.js
+++ public/script.js
@@ -1,26 +1,3 @@
-// function convertCat(nameList) {
-    // process your restaurants here!
-    // const catList = [];
-    // const catObj = {};
-    // var i;
-    // for (let i=0; i<nameList.length; i++) {
-    //   catList.push(nameList[i].category);
-    //   if (!catObj[catList[i]]) {
-    //     catObj[catList[i]] = 0;
-    //   }
-    //   catObj[catList[i]] += 1;
-    //   // console.log(restaurantList[i].category);
-    // }
-    // //   console.log(catObj);
-    // const list = Object.keys(catObj).map((m) => ({
-    //   y: catObj[m],
-    //   label: m
-    // }));
-  
-    // return list;
-
-
-//   }
 
 const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 const names = [];
