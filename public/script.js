56fc19ccc7a1c8578878103d4e98c6207fb15036
Carl Jeanty <CarlJeanty@users.noreply.github.com>
2020-11-07 10:38:28 -0500

    added aaethi update to my branch

--- public/script.js
+++ public/script.js
@@ -1,8 +1,21 @@
+function convertNamesToCategories(nameCatList) {
+    // process your restaurants here!
+    const nameList = [];
+    const nameObj = {};
+    var i;
+    for (let i=0; i<nameCatList.length; i++) {
+        nameList.push(nameCatList[i].category);
+      if (!nameObj[nameList[i]]) {
+        nameObj[nameList[i]] = 0;
+      }
+      nameObj[nameList[i]] += 1;
+      console.log(restaurantList[i].category);
+    }
+    //   console.log(catObj);
+    const list = Object.keys(nameObj).map((m) => ({
+      y: nameObj[m],
+      label: m
+    }));
 
-
-function getRestaurantArr(jsonFromServer){
-    console.log('jsonFromServer',jsonFromServer);
-    
-    const name = [];
-    fetch(jsonFromServer).then()
-}
+    return list;
+  } 
